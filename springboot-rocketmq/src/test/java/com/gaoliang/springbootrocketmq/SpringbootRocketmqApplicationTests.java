062c8077ecc15cb46cc6fd7c5652f02def8ce7e4
GaoLiang <178697490@qq.com>
2020-04-29 09:07:33 +0800

    init files

---
+++ springboot-rocketmq/src/test/java/com/gaoliang/springbootrocketmq/SpringbootRocketmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoliang.springbootrocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
